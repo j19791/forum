6434f5e0f62af9ea69ca3ef6b1a4e35f5fdfba92
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2019-08-06 09:09:52 -0300

    Aula 2 - Utilizando DevTools, padrão DTO

--- README.md
+++ README.md
@@ -28,3 +28,24 @@ Eclipse: Import / Existing Maven Projects
 Na classe Main ForumApplication.java :  Run As > Java Application
 
 Pagina do projeto (http://localhost:8080/)
+
+
+## Dependencia Devtools
+
+Permite reinicialização automatica do servidor sem necessidade de reinicia-lo ao salvar as mudanças
+
+## Padrão DTO
+
+Utilizando JPA, não se deve retornar todos os atributos de um,a entidade JPA. Com DTO, você escolhe exatamente os atributos da classe que você deseja retornar na resposta
+
+## REST
+
+* Representational State Transfer
+* Modelo de Arquitetura para sistemas distribuidos
+* Utiliza o protocolo HTTP
+* Evolução do modelo SOAP (xml)
+* Recursos: o que a aplicação gerencia - Alunos, tópicos, respostas, cursos
+* URI: identificação única dos recursos: /alunos, /topicos, /respostas
+* Verbos: manipulação dos recursos: GET /alunos POST /alunos PUT /alunos{id} 
+* Representação dos recursos : media type : XML, JSON
+* Stateless: não guarda sessão - não armazena dados dos usuários p/ que o protocolo tenha escalabilidade
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,23 +3,25 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController //Não precisa utilizar a anotação ResponseBody - todos os metodos retornam por padrão no corpo da resposta
 public class TopicosController {
 
-	@RequestMapping("/topicos") ////Endpoint p/ carregar uma lista de todos os topicos
-	@ResponseBody
-	public List<Topico> lista(){ 
+	@RequestMapping("/topicos") ////Endpoint p/ carregar uma lista de todos os topicos	
+	public List<TopicoDTO> lista(){ //padrão DTO: nao retorna todos os atributos da entidade JPA
 		
 		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);//retorna uma lista com 3 elementos com o mesmo objeto topico
+		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+		
+		
+		
 		
 		
 	}
